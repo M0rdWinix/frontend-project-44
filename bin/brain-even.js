541d945a420765c57341007ded2154b79953c375
M0rdWinix <Cava13411@gmail.com>
2026-03-30 17:24:30 +0700

    baseJS

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,7 +4,7 @@ const condition = () => {
     console.log('Answer "yes" if the number is even, otherwise answer "no".')
 }
 
-const question = () => {
+const question = () => {//сделай цикл я хз чтобы он три раза повторялся, переменную хз - карчое думай//
     let number = Math.floor(Math.random() * 10)
     let correctAnswer = "yes"
     if (number % 2 === 0) {
@@ -15,7 +15,7 @@ const question = () => {
     const answer = readlineSync.question('Your answer: ' );
     if (answer === correctAnswer) {
         console.log('Correct!')
-    } else console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet\'s try again ${}`)
+    } else console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet\'s try again`)
 }
 
 export {condition, question}
