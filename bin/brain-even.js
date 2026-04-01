5e9633f0911e170347ab924ada5e829935ed3625
M0rdWinix <Cava13411@gmail.com>
2026-04-01 10:59:14 +0700

    stage5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,21 +1,13 @@
-import readlineSync from 'readline-sync'
+#!/usr/bin/env node
+import getName from '../src/cli.js'
+import { condition, question } from '../src/functions.js'
 
-const condition = () => {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".')
+function app() {
+  console.log('Welcome to the Brain Games!')
+
+  getName()
+  condition()
+  question()
 }
 
-const question = () => {//сделай цикл я хз чтобы он три раза повторялся, переменную хз - карчое думай//
-    let number = Math.floor(Math.random() * 10)
-    let correctAnswer = "yes"
-    if (number % 2 === 0) {
-         correctAnswer = "yes"
-    } else correctAnswer = "no"
-
-    console.log(`Question: ${number}`)
-    const answer = readlineSync.question('Your answer: ' );
-    if (answer === correctAnswer) {
-        console.log('Correct!')
-    } else console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet\'s try again`)
-}
-
-export {condition, question}
+app()
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import getName from '../src/cli.js'
-import {condition, question} from '../bin/brain-even.js'
+import { condition, question } from '../src/functions.js'
 
 function app() {
   console.log('Welcome to the Brain Games!')
